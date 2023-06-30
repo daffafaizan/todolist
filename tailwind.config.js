15bdf069a41f9c51e46ac8ebd828b36a6e0a9d2a
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-06-30 16:43:12 +0700

    Finished setting tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["inter", "serif"],
+      },
+      colors: {
+        "after-midnight": "#30343f",
+      },
+      screens: {
+        xxxs: { min: "100px", max: "399px" },
+        xxs: { min: "400px", max: "575px" },
+        sm: { min: "576px", max: "767px" },
+        md: { min: "768px", max: "991px" },
+        lg: { min: "992px", max: "1199px" },
+        xl: { min: "1200px" },
+      },
+    },
   },
   plugins: [],
 };
